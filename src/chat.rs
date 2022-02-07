4e2fb574d787c43caae38beb89bf4cb453298a86
Rayyan Iqbal <r.iqbal@mediaadvancements.com>
2022-02-07 20:26:54 +0000

    Some improved code for the enumeration of events

--- src/chat.rs
+++ src/chat.rs
@@ -1,18 +1,57 @@
 use std::error::Error;
 use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
+use std::net::{Shutdown, TcpListener, TcpStream};
+
+pub enum Event {
+    Connected { username: String },
+    SetUsername { username: String },
+    Disconnected { username: String },
+    CreateRoom { roomId: String },
+    JoinRoom { roomId: String },
+}
+
+pub enum Commands {
+    SetUsername,
+}
+
+impl Commands {
+    pub fn value(&self) -> String {
+        match self {
+            Commands::SetUsername => String::from("!username"),
+        }
+    }
+}
+
+pub struct EventHandler {}
+
+impl EventHandler {
+    pub fn handle_event(event: Event) {
+        println!("Tiggering the event handler!");
+        match event {
+            Event::Connected { username } => {}
+            Event::Disconnected { username } => todo!(),
+            Event::CreateRoom { roomId } => todo!(),
+            Event::JoinRoom { roomId } => todo!(),
+            Event::SetUsername { username } => {
+                println!("The user is trying to change their name to: {}", username);
+            }
+        }
+    }
+}
 
 pub struct Server {
     pub host: String,
     address: String,
     port: String,
     listener: TcpListener,
+
     pub connected_clients: u64,
 }
 
 impl Server {
     pub fn new(address: String, port: String) -> Result<Server, std::io::Error> {
         let mut host = address.clone();
+
         host.push(':');
         host.push_str(&port);
 
@@ -22,6 +61,7 @@ impl Server {
             address,
             port,
             listener,
+
             connected_clients: 0,
         })
     }
@@ -29,26 +69,48 @@ impl Server {
     pub fn connection_handler(mut stream: TcpStream) {
         let mut data = [0 as u8; 50]; // using 50 byte buffer
 
-        std::thread::spawn(move || loop {
-            match stream.read(&mut data) {
+        std::thread::spawn(move || {
+            while match stream.read(&mut data) {
                 Ok(size) => {
                     let data = data[..size].to_vec();
+                    let message = String::from_utf8(data).unwrap();
+
+                    if (message.len() > 0) {
+                        println!("Recieved a message: {:?}", message);
+
+                        // Begin event handle
+
+                        if message.starts_with(&Commands::SetUsername.value()) {
+                            let username =
+                                message.replace("!username", "").trim_start().to_string();
+                            let event = Event::SetUsername { username };
+                            EventHandler::handle_event(event);
+                        }
+                    }
+
+                    true
                 }
-                Err(_) => todo!(),
-            }
+                Err(_) => {
+                    stream.shutdown(Shutdown::Both).unwrap();
+                    true
+                }
+            } {}
         });
     }
 
     pub fn start_listening(&mut self) {
-        
         for stream in self.listener.incoming() {
             match stream {
                 Ok(stream) => {
                     self.connected_clients += 1;
+                    println!(
+                        "A new client has connected! There are now {} connected clinets",
+                        self.connected_clients
+                    );
                     Self::connection_handler(stream);
                 }
                 Err(e) => {
-z
+                    panic!("Uh oh!")
                 }
             }
         }
--- src/cli.rs
+++ src/cli.rs
@@ -4,14 +4,11 @@ use std::net::{TcpListener, TcpStream};
 
 use crate::chat;
 
-
-
-
 pub fn main() -> std::io::Result<()> {
     let address = String::from("127.0.0.1");
-    let port = String::from("80");
+    let port = String::from("3040");
 
-    let server = chat::Server::new(address, port)?;
+    let mut server = chat::Server::new(address, port)?;
 
     server.start_listening();
 
